b375a7aba22564838fd489ec7ce10c64f8a10007
Duc Anh Babim <babim@matmagoc.com>
2017-09-03 02:03:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+CONFIG=${CONFIG:-/opt/ether-proxy/ether-proxy/config.json}
 if [ -z "$CONFIG_PATH" ]; then
     echo "Need to set CONFIG_PATH"
     exit 1
 fi
 
-/opt/ether-proxy/ether-proxy $CONFIG_PATH
+/opt/ether-proxy/ether-proxy $CONFIG
